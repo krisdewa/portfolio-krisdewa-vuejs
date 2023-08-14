925b322aebfe96d11071f02d5760e6c3dab07342
Krisna Dewa <59174531+krisdewa@users.noreply.github.com>
2023-08-14 21:52:01 +0700

    replace image hero & fix error skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,78 +1,34 @@
 <template>
-        <!-- SKILLS -->
         <div class="skills" id="skills">
                 <div class="container">
                         <h2 class="skillsh2">Skills</h2>
                         <span class="line rounded-pill"></span>
-                        <!-- CODE SKILL -->
-                        <!-- <div class="backskill"> -->
                         <div class="programming text-center">
-                                <a href="https://www.php.net/" target="_blank"><img src="frontend/icon/php.svg"
-                                                class="php" alt="php" /></a>
-                                <a href="https://laravel.com/" target="_blank"><img src="frontend/icon/laravel.svg"
-                                                class="laravel" alt="laravel" /></a>
-                                <a href="https://codeigniter.com/" target="_blank"><img
-                                                src="frontend/icon/codeigniter.svg" class="img-svg"
-                                                alt="codeigniter" /></a>
-                                <a href="https://www.python.org/" target="_blank"><img src="frontend/icon/python.svg"
-                                                class="img-svg" alt="py" /></a>
-                                <a href="https://www.mysql.com/" target="_blank"><img src="frontend/icon/mysql.svg"
-                                                class="img-svg" alt="mysql" /></a>
-                                <a href="https://id.wikipedia.org/wiki/HTML" target="_blank"><img
-                                                src="frontend/icon/html.svg" alt="html" /></a>
-                                <a href="https://en.wikipedia.org/wiki/CSS" target="_blank"><img
-                                                src="frontend/icon/css.svg" alt="css" /></a>
-                                <a href="https://id.wikipedia.org/wiki/JavaScript" target="_blank"><img
-                                                src="frontend/icon/javascript.svg" alt="javascript" /></a>
-                                <a href="https://getbootstrap.com/" target="_blank"><img
-                                                src="frontend/icon/bootstrap.svg" class="img-svg" alt="bootstrap" /></a>
-                                <a href="https://reactjs.org/" target="_blank"><img src="frontend/icon/reactjs.svg"
-                                                class="img-svg" alt="reactjs" /></a>
-                                <a href="https://vuejs.org/" target="_blank"><img src="frontend/icon/vuejs.svg"
-                                                class="img-svg" alt="vuejs" /></a>
-                                <!-- <img src="assets/icon/git.svg" alt="git" /> -->
-                                <!-- <img src="assets/icon/github.svg" alt="php" /> -->
+                                <!-- Loop through skills array to generate skill icons -->
+                                <a v-for="skill in skills" :key="skill.name" :href="skill.link" target="_blank">
+                                        <img :src="skill.icon" :alt="skill.alt" class="img-svg" />
+                                </a>
                         </div>
-                        <!-- </div> -->
-                        <!-- END CODE SKILL -->
-
                         <!-- TOOLS -->
                         <h2 class="toolsh2 mt-5">Tools</h2>
                         <span class="line rounded-pill"></span>
-                        <!-- <div class="backtool"> -->
                         <div class="tools text-center">
-                                <!-- <img src="./assets/icon/photoshop.svg" alt="photoshop" /> -->
-                                <a href="https://code.visualstudio.com/" target="_blank">
-                                        <img class="img-svg" src="frontend/icon/vscode.svg" alt="vscode" />
+                                <!-- Loop through tools array to generate tool icons -->
+                                <a v-for="tool in tools" :key="tool.name" :href="tool.link" target="_blank">
+                                        <img :src="tool.icon" :alt="tool.alt" class="img-svg" />
                                 </a>
-                                <a href="https://filmora.wondershare.net/filmora-video-editor.html?gclid=CjwKCAjw9aiIBhA1EiwAJ_GTSkhAwcp5LzvwB2z2vAAr5zck5z_1TaB8jGz3r0kQn_b8gMcRWJhlZRoCT3gQAvD_BwE"
-                                        target="_blank"><img class="img-svg" src="frontend/icon/filmora.svg"
-                                                alt="filmora" /></a>
-                                <a href="https://www.figma.com" target="_blank"><img class="img-svg"
-                                                src="frontend/icon/figma.svg" alt="figma" /></a>
-                                <a href="https://www.jetbrains.com/phpstorm/" target="_blank"><img class="img-svg"
-                                                src="frontend/icon/phpstorm.svg" alt="phpstorm" /></a>
-                                <a href="https://www.jetbrains.com/idea/" target="_blank"><img class="img-svg"
-                                                src="frontend/icon/IntelliJ_idea.svg" alt="phpstorm" /></a>
                         </div>
-                        <!-- </div> -->
-                        <!-- END TOOLS -->
                 </div>
         </div>
-        <!-- SKILL & TOOL -->
 </template>
-
+      
 <script>
-import ref from 'vue'
+import { ref } from 'vue';
 
 export default {
-        name: "SkillComponent",
-        async setup() {
-                const skills = ref([])
-
-                // await new Promise(resolve => setTimeout(resolve, 5000));
-
-                skills.value = [
+        name: 'SkillComponent',
+        setup() {
+                const skills = ref([
                         {
                                 name: "PHP",
                                 icon: "frontend/icon/php.svg",
@@ -138,49 +94,53 @@ export default {
                                 icon: "frontend/icon/vuejs.svg",
                                 link: "https://vuejs.org/",
                                 alt: "vuejs",
+                        }
+                ]);
+
+                const tools = ref([
+                        {
+                                name: 'Visual Studio Code',
+                                icon: 'frontend/icon/vscode.svg',
+                                link: 'https://code.visualstudio.com/',
+                                alt: 'vscode',
                         },
                         {
-                                name: "VSCode",
-                                icon: "frontend/icon/vscode.svg",
-                                link: "https://code.visualstudio.com/",
-                                alt: "vscode",
+                                name: 'Filmora',
+                                icon: 'frontend/icon/filmora.svg',
+                                link: 'https://filmora.wondershare.net/filmora-video-editor.html?gclid=CjwKCAjw9aiIBhA1EiwAJ_GTSkhAwcp5LzvwB2z2vAAr5zck5z_1TaB8jGz3r0kQn_b8gMcRWJhlZRoCT3gQAvD_BwE',
+                                alt: 'filmora',
                         },
                         {
-                                name: "Filmora",
-                                icon: "frontend/icon/filmora.svg",
-                                link: "https://filmora.wondershare.net/filmora-video-editor.html?gclid=CjwKCAjw9aiIBhA1EiwAJ_GTSkhAwcp5LzvwB2z2vAAr5zck5z_1TaB8jGz3r0kQn_b8gMcRWJhlZRoCT3gQAvD_BwE",
-                                alt: "filmora",
+                                name: 'Figma',
+                                icon: 'frontend/icon/figma.svg',
+                                link: 'https://www.figma.com',
+                                alt: 'figma',
                         },
                         {
-                                name: "Figma",
-                                icon: "frontend/icon/figma.svg",
-                                link: "https://www.figma.com",
-                                alt: "figma",
+                                name: 'PhpStorm',
+                                icon: 'frontend/icon/phpstorm.svg',
+                                link: 'https://www.jetbrains.com/phpstorm/',
+                                alt: 'phpstorm',
                         },
                         {
-                                name: "PhpStorm",
-                                icon: "frontend/icon/phpstorm.svg",
-                                link: "https://www.jetbrains.com/phpstorm/",
-                                alt: "phpstorm",
+                                name: 'IntelliJ Idea',
+                                icon: 'frontend/icon/IntelliJ_idea.svg',
+                                link: 'https://www.jetbrains.com/idea/',
+                                alt: 'intellij',
                         },
-                        {
-                                name: "IntelliJ Idea",
-                                icon: "frontend/icon/IntelliJ_idea.svg",
-                                link: "https://www.jetbrains.com/idea/",
-                                alt: "phpstorm",
-                        },
-                ]
+                ]);
 
                 return {
                         skills,
-                }
+                        tools,
+                };
         },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+      
+<style scoped>
 .img-svg {
         max-width: 110px;
 }
 </style>
+      
